e0b9a0acf3620c690b22f47e58cbed4fcbcb5a07
Gaurav Tak <gauravtak996@gmail.com>
2023-07-11 15:03:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+anime({
+    targets: '.hero-text-1',
+    translateY: [450, -50], // from 100 to 250
+    delay: 50,
+    easing: 'easeOutCubic',
+    //   direction: 'alternate',
+    //   loop: true
+});
+
+anime({
+    targets: '.hero-text-2',
+    translateY: [450, -45], // from 100 to 250
+    delay: 100,
+    easing: 'easeOutCubic',
+    //   direction: 'alternate',
+    //   loop: true
+});
+
+anime({
+    targets: '.hero-text-3',
+    translateY: [450, -30], // from 100 to 250
+    delay: 700,
+    easing: 'easeOutCubic',
+    //   direction: 'alternate',
+    //   loop: true
+});
