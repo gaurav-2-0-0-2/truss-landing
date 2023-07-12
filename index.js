eaae30dc9b85dd980a6127cd6b0c7b468047a1c5
Gaurav Tak <gauravtak996@gmail.com>
2023-07-12 15:31:42 +0530

    improved hero-text animation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 anime({
     targets: '.hero-text-1',
-    translateY: [450, -15], // from 100 to 250
-    delay: 50,
+    translateY: [650, -15], // from 100 to 250
+    delay: 650,
     easing: 'easeOutCubic',
     //   direction: 'alternate',
     //   loop: true
@@ -9,8 +9,8 @@ anime({
 
 anime({
     targets: '.hero-text-2',
-    translateY: [450, -10], // from 100 to 250
-    delay: 100,
+    translateY: [650, -10], // from 100 to 250
+    delay: 1350,
     easing: 'easeOutCubic',
     //   direction: 'alternate',
     //   loop: true
@@ -18,9 +18,17 @@ anime({
 
 anime({
     targets: '.hero-text-3',
-    translateY: [450, -5], // from 100 to 250
-    delay: 700,
+    translateY: [650, -5], // from 100 to 250
+    delay: 1500,
     easing: 'easeOutCubic',
     //   direction: 'alternate',
     //   loop: true
 });
+
+
+anime({
+    targets: '.block-3_sub',
+    // translateY: [1700, 5],
+    delay: 50,
+    easing: 'easeOutCubic'
+})
